5060a763f80cd8f97faa1b9b2075eac5d7978154
Bryan Hann <nobody@nowhere>
2026-09-20 16:10:46 -0230

    R: Junk a function used only with regression testing.

--- tests/lib/functions.sh
+++ tests/lib/functions.sh
@@ -12,20 +12,6 @@ err4err () {
     rm $err
 }
     
-my_regress () {
-    # XXX This was refactored without regression testing.
-    local fun=${FUNCNAME[1]}
-    local exp_stdout=$KT_EXP/$fun.out
-    local exp_stderr=$KT_EXP/$fun.err
-    local got_stdout=$KT_GOT/$fun.out
-    local got_stderr=$KT_GOT/$fun.err
-    $* > $got_stdout 2> $got_stderr
-    err=0
-    cmp $got_stdout $exp_stdout > /dev/null || err=$(( $err + 1 ))
-    cmp $got_stderr $exp_stderr > /dev/null || err=$(( $err + 2 ))
-    return $err
-}
-
 fatal () {
     . .k0.bold "    ${FUNCNAMS[1]}: fatal: $*" >&1
     return 1

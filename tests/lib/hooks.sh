b7894baefc250477f9e2d01eea00cd414c7efe79
Bryan Hann <nobody@nowhere>
2026-09-20 20:05:43 -0230

    Improve.

--- tests/lib/hooks.sh
+++ tests/lib/hooks.sh
@@ -1,10 +1,10 @@
 oneTimeSetUp() {
    # XXX This was refactored without regression testing.
    mkdir -p $KT_SANDBOX
-   rm       $KT_GOT/*
-   rmdir    $KT_GOT
-   mkdir -p $KT_EXP
-   mkdir -p $KT_GOT
+#   rm       $KT_GOT/*
+#   rmdir    $KT_GOT
+#   mkdir -p $KT_EXP
+#   mkdir -p $KT_GOT
 }
 
 oneTimeTearDown() {
